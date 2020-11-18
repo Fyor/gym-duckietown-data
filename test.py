8511d62e93095c668d8cc6cce16595fcf917b487
thomas <thomas.wiggers@student.uva.nl>
2020-11-18 16:58:34 +0100

    old test changes?

--- test.py
+++ test.py
@@ -6,8 +6,13 @@ import numpy as np
 import gym
 import torch
 import torch.nn as nn
+from tqdm import trange
 
 from train import Env, Net
+
+import gym_duckietown
+
+from train_ray import velangle_to_lrpower
 
 parser = argparse.ArgumentParser(description='Test the PPO agent for the CarRacing-v0')
 parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 12)')
@@ -21,118 +26,6 @@ device = torch.device("cuda" if use_cuda else "cpu")
 torch.manual_seed(args.seed)
 if use_cuda:
     torch.cuda.manual_seed(args.seed)
-
-#
-# class Env():
-#     """
-#     Test environment wrapper for CarRacing
-#     """
-#
-#     def __init__(self):
-#         self.env = gym.make('CarRacing-v0')
-#         self.env.seed(args.seed)
-#         self.reward_threshold = self.env.spec.reward_threshold
-#
-#     def reset(self):
-#         self.counter = 0
-#         self.av_r = self.reward_memory()
-#
-#         self.die = False
-#         img_rgb = self.env.reset()
-#         img_gray = self.rgb2gray(img_rgb)
-#         self.stack = [img_gray] * args.img_stack
-#         return np.array(self.stack)
-#
-#     def step(self, action):
-#         total_reward = 0
-#         for i in range(args.action_repeat):
-#             img_rgb, reward, die, _ = self.env.step(action)
-#             # don't penalize "die state"
-#             if die:
-#                 reward += 100
-#             # green penalty
-#             if np.mean(img_rgb[:, :, 1]) > 185.0:
-#                 reward -= 0.05
-#             total_reward += reward
-#             # if no reward recently, end the episode
-#             done = True if self.av_r(reward) <= -0.1 else False
-#             if done or die:
-#                 break
-#         img_gray = self.rgb2gray(img_rgb)
-#         self.stack.pop(0)
-#         self.stack.append(img_gray)
-#         assert len(self.stack) == args.img_stack
-#         return np.array(self.stack), total_reward, done, die
-#
-#     def render(self, *arg):
-#         self.env.render(*arg)
-#
-#     @staticmethod
-#     def rgb2gray(rgb, norm=True):
-#         gray = np.dot(rgb[..., :], [0.299, 0.587, 0.114])
-#         if norm:
-#             # normalize
-#             gray = gray / 128. - 1.
-#         return gray
-#
-#     @staticmethod
-#     def reward_memory():
-#         count = 0
-#         length = 100
-#         history = np.zeros(length)
-#
-#         def memory(reward):
-#             nonlocal count
-#             history[count] = reward
-#             count = (count + 1) % length
-#             return np.mean(history)
-#
-#         return memory
-#
-#
-# class Net(nn.Module):
-#     """
-#     Actor-Critic Network for PPO
-#     """
-#
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.cnn_base = nn.Sequential(  # input shape (4, 96, 96)
-#             nn.Conv2d(args.img_stack, 8, kernel_size=4, stride=2),
-#             nn.ReLU(),  # activation
-#             nn.Conv2d(8, 16, kernel_size=3, stride=2),  # (8, 47, 47)
-#             nn.ReLU(),  # activation
-#             nn.Conv2d(16, 32, kernel_size=3, stride=2),  # (16, 23, 23)
-#             nn.ReLU(),  # activation
-#             nn.Conv2d(32, 64, kernel_size=3, stride=2),  # (32, 11, 11)
-#             nn.ReLU(),  # activation
-#             nn.Conv2d(64, 128, kernel_size=3, stride=1),  # (64, 5, 5)
-#             nn.ReLU(),  # activation
-#             nn.Conv2d(128, 256, kernel_size=3, stride=1),  # (128, 3, 3)
-#             nn.ReLU(),  # activation
-#         )  # output shape (256, 1, 1)
-#         self.v = nn.Sequential(nn.Linear(256, 100), nn.ReLU(), nn.Linear(100, 1))
-#         self.fc = nn.Sequential(nn.Linear(256, 100), nn.ReLU())
-#         self.alpha_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
-#         self.beta_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
-#         self.apply(self._weights_init)
-#
-#     @staticmethod
-#     def _weights_init(m):
-#         if isinstance(m, nn.Conv2d):
-#             nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
-#             nn.init.constant_(m.bias, 0.1)
-#
-#     def forward(self, x):
-#         x = self.cnn_base(x)
-#         x = x.view(-1, 256)
-#         v = self.v(x)
-#         x = self.fc(x)
-#         alpha = self.alpha_head(x) + 1
-#         beta = self.beta_head(x) + 1
-#
-#         return (alpha, beta), v
-
 
 class Agent():
     """
@@ -151,37 +44,91 @@ class Agent():
         action = action.squeeze().cpu().numpy()
         return action
 
-    def load_param(self):
-        self.net.load_state_dict(torch.load('param/checkpoint.pkl', map_location=device))
-
+    def load_param(self, file=None):
+        # self.net.load_state_dict(torch.load('param/checkpoint.pkl', map_location=device))
+        # self.net.load_state_dict(torch.load('param/ppo_net_params_zigzag_fast_0.25.pkl', map_location=device)) # 0.3 speed
+        file = file or 'param/ppo_net_params_check_straight_no_l2.pkl'
+        self.net.load_state_dict(torch.load(file, map_location=device))
 DUCKIETOWN = True
 
+# "loop_empty  small_loop_cw  small_loop  straight_road   udem1  zigzag_dists"
 if __name__ == "__main__":
-    agent = Agent()
-    agent.load_param()
-    env = Env()
+    table = []
+    # for agent_weights, agent_speed in [('param/ppo_net_params_zigzag_fast_0.25.pkl', 0.25)]: #[ ('param/checkpoint.pkl', 0.05)]: # ('param/ppo_net_params_check_straight_no_l2.pkl', 0.2), ('param/ppo_net_params_zigzag_fast_0.25.pkl', 0.3),
+    for agent_weights, agent_speed in [('param/checkpoint.pkl', 0.25)]: #[ ('param/checkpoint.pkl', 0.05)]: # ('param/ppo_net_params_check_straight_no_l2.pkl', 0.2), ('param/ppo_net_params_zigzag_fast_0.25.pkl', 0.3),
+        for map_name in ["udem1", "zigzag_dists", "small_loop", "loop_empty"]:
 
-    training_records = []
-    running_score = 0
-    state = env.reset()
-    for i_ep in range(10):
-        score = 0
-        state = env.reset()
 
-        for t in range(1000):
-            action = agent.select_action(state)
-            if DUCKIETOWN:
-                action[0] = 0.25 # 0.1 # Fixed speed
-                state_, reward, done, info = env.step(action * np.array([1., 2.]) + np.array([0., -1.]))
-            else:
-                state_, reward, done, info = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
-            if args.render:
-                env.render()
-                time.sleep(1/40)
-            score += reward
-            state = state_
-            if done:
-                print()
-                break
+            agent = Agent()
+            agent.load_param(file=agent_weights)
+            # env = Env(maps=["zigzag_dists", "small_loop", "loop_empty"], action_repeat=8)
+            seed = np.random.randint(0,100)
+            print("seed", seed)
+            env = Env(maps=[map_name], action_repeat=2, seed=seed)
 
-        print('Ep {}\tScore: {:.2f}\t'.format(i_ep, score))
+
+            training_records = []
+            running_score = 0
+            state = env.reset()
+            lane_dist = []
+            deaths = 0
+            deaths_after_20 = 0
+            good = 0
+            ts = []
+
+            for i_ep in trange(500):
+                score = 0
+                state = env.reset()
+
+                try:
+                    for t in range(500):
+                        action = agent.select_action(state)
+                        if DUCKIETOWN:
+                            # veldir
+                            action[0] = agent_speed  # 0.3 or 0.1 # Fixed speed
+                            action = action * np.array([1., 2.]) + np.array([0., -1.])
+                            # lr power
+                            action = velangle_to_lrpower(action)
+                            # exec
+                            state_, reward, done, info = env.step(action )
+
+                            sim = env.env
+                            lp = sim.get_lane_pos2(sim.cur_pos, sim.cur_angle)
+                            d = np.abs(lp.dist)
+                            lane_dist.append(d)
+
+                        else:
+                            state_, reward, done, info = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+                        if args.render:
+                            env.render()
+                            time.sleep(1/40)
+                        score += reward
+                        state = state_
+                        if done:
+                            deaths += 1
+                            if t > 10:
+                                deaths_after_20 += 1
+                                good += 1
+                                ts.append(t)
+                            break
+                    else:
+                        good += 1
+                        ts.append(t)
+
+                    if good > 100:
+                        break
+                except gym_duckietown.simulator.NotInLane as e:
+                    print(e)
+
+                # print(env.env.map_name, f'Ep {i_ep}\tScore: {score:.2f}\tLen:{t}')
+
+            d = lane_dist
+
+            print("Map", map_name, agent_weights, agent_speed)
+            print(f"DATA: map deaths mean var & {map_name} & {deaths_after_20} & {(np.mean(d) * 100) / 0.585:.1f} \\%  & {(np.std(d)* 100) / 0.585:.1f} \\% \\ \\    Tile size 0.585")
+            table.append(f" {agent_weights} & {agent_speed*1.4:.4f}m/s & {map_name} & {deaths_after_20}/100 & {(np.mean(d) * 100) / 0.585:.1f} \\%  & {(np.std(d)* 100) / 0.585:.1f} \\% \\\\ ")
+            print(f" (Total deaths including sub20 steps {deaths}) mean steps before death {np.mean(ts)}")
+            print()
+
+    print("TABLE")
+    print("\n".join(table))
